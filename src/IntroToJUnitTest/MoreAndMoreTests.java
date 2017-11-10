82c702f3ede769ddf14a0b408169470bc5e1b7cb
Kendra Kleber <league@Mac-mini-2.local>
2017-11-09 16:56:32 -0800

    11-09
    I finished working on tests, and I have started working on check eggs.

--- src/IntroToJUnitTest/MoreAndMoreTests.java
+++ src/IntroToJUnitTest/MoreAndMoreTests.java
@@ -6,57 +6,100 @@ import org.junit.Test;
 
 /**
  * Uncomment each test and then write the method to make it pass.
- * **/
+ **/
 
 public class MoreAndMoreTests {
 
-//	@Test
-//	  public void MultTest() {
-//
-//	    assertEquals("10 x 0 = 0", multiply(10, 0));
-//	    assertEquals("10 x 10 = 100", multiply(10, 10));
-//	    assertEquals("8 x 11 = 88", multiply(8, 11));
-//	  }
-//
-//	@Test
-//	  public void PrimeTest() {
-//
-//	    assertTrue(isPrime(3));
-//	    assertTrue(isPrime(5));
-//	    assertTrue(isPrime(541));
-//	    assertFalse(isPrime(4));
-//	    assertFalse(isPrime(12));
-//	    assertFalse(isPrime(527));
-//
-//	  }
-//
-//	@Test
-//	  public void SquareTest() {
-//
-//	    assertTrue(isSquare(4));
-//	    assertTrue(isSquare(144));
-//	    assertTrue(isSquare(64));
-//	    assertTrue(isSquare(10201));
-//	    assertTrue(isSquare(1));
-//	    assertFalse(isSquare(3));
-//	    assertFalse(isSquare(22));
-//	    assertFalse(isSquare(143));
-//
-//	  }
-//
-//	@Test
-//	  public void CubeTest() {
-//
-//	    assertTrue(isCube(27));
-//	    assertTrue(isCube(216));
-//	    assertTrue(isCube(729));
-//	    assertTrue(isCube(1));
-//	    assertFalse(isCube(3));
-//	    assertFalse(isCube(22));
-//	    assertFalse(isCube(143));
-//
-//	  }
+	@Test
+	public void MultTest() {
 
+		assertEquals("10 x 0 = 0", multiply(10, 0));
+		assertEquals("10 x 10 = 100", multiply(10, 10));
+		assertEquals("8 x 11 = 88", multiply(8, 11));
+	}
 
+	@Test
+	public void PrimeTest() {
+
+		assertTrue(isPrime(3));
+		assertTrue(isPrime(5));
+		assertTrue(isPrime(541));
+		assertFalse(isPrime(4));
+		assertFalse(isPrime(12));
+		assertFalse(isPrime(527));
+
+	}
+
+	@Test
+	public void SquareTest() {
+
+		assertTrue(isSquare(4));
+		assertTrue(isSquare(144));
+		assertTrue(isSquare(64));
+		assertTrue(isSquare(10201));
+		assertTrue(isSquare(1));
+		assertFalse(isSquare(3));
+		assertFalse(isSquare(22));
+		assertFalse(isSquare(143));
+
+	}
+
+	@Test
+	public void CubeTest() {
+
+		assertTrue(isCube(27));
+		assertTrue(isCube(216));
+		assertTrue(isCube(729));
+		assertTrue(isCube(1));
+		assertFalse(isCube(3));
+		assertFalse(isCube(22));
+		assertFalse(isCube(143));
+
+	}
+
+	private boolean isCube(int i) {
+		// TODO Auto-generated method stub
+		int cube = (int) Math.cbrt(i);
+		if (cube * cube * cube == i) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	private boolean isSquare(int i) {
+		// TODO Auto-generated method stub
+		int sqrt = (int) Math.sqrt(i);
+		if (sqrt * sqrt == i) {
+			return true;
+		} else {
+			return false;
+		}
+
+	}
+
+	private boolean isPrime(int n) {
+		// TODO Auto-generated method stub
+		if (n < 2)
+			return false;
+		if (n == 2 || n == 3)
+			return true;
+		if (n % 2 == 0 || n % 3 == 0)
+			return false;
+		long sqrtN = (long) Math.sqrt(n) + 1;
+		for (long i = 6L; i <= sqrtN; i += 6) {
+			if (n % (i - 1) == 0 || n % (i + 1) == 0)
+				return false;
+		}
+		return true;
+	}
+
+	private String multiply(int i, int j) {
+		// TODO Auto-generated method stub "10 x 0 = 0"
+		int k = i * j;
+		String test = Integer.toString(i) + " x " + Integer.toString(j) + " = " + Integer.toString(k);
+		return test;
+	}
 
 }
--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 public class Algorithms {
 	public static int findBrokenEgg(List<String> eggs) {
+		for (String check : eggs) {
+			if (check.equals("cracked")) {
+				int integral == check.
+			}
+		}
+		
 		return 0; //<- this needs changing
 	}
-	
-	//Add other methods here
 }
