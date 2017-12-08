6783c19a0807d5a4d88718ec92fae3f772775f74
OrigamiMM <league@Mac-mini-3.local>
2017-12-07 16:57:05 -0800

    SearchingAndSortingAlgorithmsPart2 ip
    Continuing the same class as last time, almost done

--- src/SearchingAndSortingAlgorithms/Algorithms.java
+++ src/SearchingAndSortingAlgorithms/Algorithms.java
@@ -23,36 +23,70 @@ public class Algorithms {
 		int numPearl = 0;
 		for (int i = 0; i < oysters.size(); i++) {
 			if (oysters.get(indexPearl)) {
-				numPearl+=1;
+				numPearl += 1;
 			}
 			indexPearl++;
 		}
 		return numPearl;
 	}
+
 	public static double findTallest(List<Double> peeps) {
 		double currentPeep = 0;
 		double myTallestPeep = 0;
 		for (int i = 0; i < peeps.size(); i++) {
 			currentPeep = peeps.get(i);
-			if (currentPeep>myTallestPeep) {
-				myTallestPeep = currentPeep; 
+			if (currentPeep > myTallestPeep) {
+				myTallestPeep = currentPeep;
 			}
 		}
-		return myTallestPeep; 
+		return myTallestPeep;
 	}
-	public static String findLongestWord(List<String>words) {
+
+	public static String findLongestWord(List<String> words) {
 		String currentWord = "";
 		String longestWord = "";
 		for (int i = 0; i < words.size(); i++) {
 			currentWord = words.get(i);
-			if (currentWord.length()>longestWord.length()) {
+			if (currentWord.length() > longestWord.length()) {
 				longestWord = currentWord;
 			}
 		}
 		return longestWord;
 	}
-	public static boolean containsSOS(List<String> message1){
-		
+
+	public static boolean containsSOS(List<String> message1) {
+		String sos = " ... --- ... ";
+		for (int i = 0; i < message1.size(); i++) {
+			if (sos.equals(message1.get(i))) {
+
+				return true;
+			}
+		}
 		return false;
+	}
+
+	public static List<Double> sortScores(List<Double> results) {
+		Double currentScore = 0.0;
+		for (int i = 0; i < results.size(); i++) {
+			for (int j = i; j < results.size(); j++) {
+				if (results.get(j) < results.get(i)) {
+					currentScore = results.get(i);
+					results.set(i, results.get(j));
+					results.set(j, currentScore);
+				}
+			}
+		}
+		return results;
+	}
+	public static List<String> sortDNA(List<String> unsortedSequences) {
+		String CurrentDNA = "";
+		for (int i = 0; i < unsortedSequences.size(); i++) {
+			for (int j = i; j < unsortedSequences.size(); j++) {
+				if (unsortedSequences.get(j).length() < unsortedSequences.get(i).length()) {
+					
+				}
+			}
+		}
+		return unsortedSequences;
 	}
 }
